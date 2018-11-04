7c406f9b2fcf4566ceced5124c984ea1a5e3ca17
Serhii Kasatkin <serhiikasatkin@MacBook-Pro-Serhii.local>
2018-11-04 13:07:27 +0200

    Added composer autoload dependencies

---
+++ src/test/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace src\test;
+
+class Test 
+{
+    
+    public function printContent ()
+    {
+        return "Namespace src\\test ; class Test; Content: !!!Hello WORLD!!!!";
+    }
+}
