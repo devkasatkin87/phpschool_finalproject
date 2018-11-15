12eac4c259254ffa2afcb9640a157589c2edb428
Serhii Kasatkin <serhiikasatkin@MacBook-Pro-Serhii.local>
2018-11-15 21:28:33 +0200

    added user routes

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'user/login' => 'user/login', //actionLogin UserController
+    'user/register' => 'user/register', //actionRegister UserController
     'article/create' => 'article/create', //actionCreate ArticleController
     'article/([0-9]+)' => 'article/view/$1', //actionView ArticleController
     'generate' => 'generate/index', //actionIndex GenerateController
