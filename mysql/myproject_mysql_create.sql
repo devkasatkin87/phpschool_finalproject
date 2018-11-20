340f73fed91ff676b709562abbc3b39019ff938f
Serhii Kasatkin <serhiikasatkin@MacBook-Pro-Serhii.local>
2018-11-20 22:21:11 +0200

    added db dump

---
+++ mysql/myproject_mysql_create.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `articles` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`title` varchar(56) NOT NULL,
+	`date_published` varchar(56) NOT NULL,
+	`content` TEXT NOT NULL,
+	`img` varchar(255) NOT NULL,
+	`views` int(11) NOT NULL,
+	`topic_id` int(11) NOT NULL,
+	`author_id` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `authors` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(56) NOT NULL,
+	`second_name` varchar(56) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `topics` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`title` varchar(56) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`username` varchar(56) NOT NULL,
+	`password` varchar(56) NOT NULL,
+	`is_admin` bool NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `articles` ADD CONSTRAINT `articles_fk0` FOREIGN KEY (`topic_id`) REFERENCES `topics`(`id`);
+
+ALTER TABLE `articles` ADD CONSTRAINT `articles_fk1` FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`);
+
